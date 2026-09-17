9913d208f1d3ff30fc15bb6be6612070c68be4ce
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-09-17 02:41:39 -0600

    feat: give indexer ability to succinctly read page text

--- services/indexer/main.py
+++ services/indexer/main.py
@@ -1,9 +1,10 @@
 import asyncio
 import asyncpg
-import time
 import logging
-import sys
 import os
+import sys
+import traceback
+from bs4 import BeautifulSoup
 
 
 logging.basicConfig(
@@ -16,29 +17,70 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# async def runIndexer(conn: connection) -> RuntimeError:
-#     while True:
-#         # Get item with the oldest time_since_last_indexed
-#         cur = conn.cursor()
-
-        
-
-#         # Index it
-        
-
-#         logger.info("Still indexing web pages...")
-#         time.sleep(1.25)
-
 async def main() -> None:
     try:
-        conn = await asyncpg.connect(user=os.getenv("DB_USERNAME"), password=os.getenv("DB_PASSWORD"), database=os.getenv("DB_NAME"), host=os.getenv("DB_HOST"))
+        conn = await asyncpg.connect(
+            user=os.getenv("DB_USERNAME"),
+            password=os.getenv("DB_PASSWORD"), 
+            database=os.getenv("DB_NAME"), 
+            host=os.getenv("DB_HOST"))
         
         while True:
-            logger.info("Still indexing web pages...")
-            time.sleep(1.25)
+            
+            values: list = await conn.fetch(
+                'SELECT * FROM indexer_queue ORDER BY id ASC LIMIT 1;'
+            )
+
+            if len(values) < 1:
+                await asyncio.sleep(1)
+                continue
+
+            pageId = values[0]['page_id']
+            siteId = values[0]['site_id']
+
+            values: list = await conn.fetch(
+                """
+                SELECT (
+                    response_body
+                )
+                FROM pages
+                WHERE id = $1
+                LIMIT 1;
+                """,
+                pageId
+            )
+
+            if len(values) < 1:
+                continue
+
+            result: asyncpg.protocol.record.Record = values[0]
+            html = next(result.values())
+            
+            soup = BeautifulSoup(html, 'html.parser')
+
+            PRETTY_HTML = soup.prettify()
+            text = soup.get_text()
+            trimmed_text = text.strip() # Removes leading and trailing whitespace
+            CLEANED_TEXT = ' '.join(trimmed_text.split()) # Removes excessive in-text whitespace
+
+            # Get keywords from CLEANED_TEXT
+
+            # In a single transaction
+                # Append webpage id to arrays in each keyword's key-value db entry
+                # Delete this page from indexer queue
+                # Add this page to ranker queue
+
+            print(CLEANED_TEXT)
+
+            break
+    
+    except Exception as error:
+        print(f"Error: {error}")
+        traceback.print_exc()
 
     finally:
-        conn.close()
+        if conn is not None:
+            await conn.close()
 
 if __name__ == '__main__':
     asyncio.run(main())
