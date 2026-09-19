02a20f190418e56bca5c9e95421ada054e409851
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-09-19 03:08:18 -0600

    fix: let `ranking_engine` service actually build

--- services/ranking_engine/src/ranking_engine/src/RankingEngine.java
+++ services/ranking_engine/src/ranking_engine/src/RankingEngine.java
@@ -1,6 +1,6 @@
 package ranking_engine.src;
 
-class RankingEngine {
+public class RankingEngine {
     public static void main(String[] args) {
         System.out.println("I'm the ranking engine! I judge webpages' importance!");
     }
