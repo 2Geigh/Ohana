2eca56372d2b1062655c65c2ff4525a290db127a
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-09-20 03:34:12 -0600

    refactor: modularize `db` service's scripts out of dockerfile
    This will make it easier to track changes and edit and whatnot

---
+++ services/db/scripts/monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+exec watch -n 0.25 '
+psql -d ohana -c "
+SELECT count(*) AS sites_count FROM sites;
+SELECT count(*) AS pages_count FROM pages;
+SELECT count(*) AS crawler_queue_count FROM crawler_queue;
+SELECT count(*) AS indexer_queue_count FROM indexer_queue;
+SELECT count(*) AS ranker_queue_count FROM ranking_engine_queue;
+"
+'
