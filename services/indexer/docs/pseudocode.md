75d6d6fbba0baced47238e9fb44502fba38e78b1
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-09-22 17:42:28 -0600

    feat(indexer): get page_id, site_id, page.link and page.response_body from indexer_queue

--- services/indexer/docs/pseudocode.md
+++ services/indexer/docs/pseudocode.md
@@ -3,21 +3,21 @@
 - Connect to Postgres container
 
 - While True:
-    - Grab the page from `pages` that has been indexed the longest time ago, so long as it hasn't been indexed within the last `TIME_PERIOD` (this time period, and the crawlers' constants should be kept in an external file that can be accessed by all services (.env?))
+  - Grab the page from `pages` that has been indexed the longest time ago, so long as it hasn't been indexed within the last `TIME_PERIOD` (this time period, and the crawlers' constants should be kept in an external file that can be accessed by all services (.env?))
 
-    - If there are no webpages found in `pages`, wait/sleep for `INDEXER_POLITENESS_DURATION`
+  - If there are no webpages found in `pages`, wait/sleep for `INDEXER_POLITENESS_DURATION`
 
-    - Extract all text chunks from the page, and additionally compile them into a single full text source
+  - Extract all text chunks from the page, and additionally compile them into a single full text source
 
-    - Run [sentence-transformers/all-MiniLM-L6-v2]("https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2") in ONNX runtime
-    
-    - Use the above model to vectorize the full page text and store the embedding in the pgvector `pages` column
+  - Run [sentence-transformers/all-MiniLM-L6-v2]("https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2") in ONNX runtime
 
-    - For each text chunk (or the whole page, depending on memory usage in testing) using [Apache Lucene]("https://lucene.apache.org/core/"):
-        - Tokenize the page text.
-        - Lowercase terms.
-        - Remove punctuation and stop words.
-        - Apply stemming or lemmatization.
-        - Index the resulting terms with Lucene.
+  - Use the above model to vectorize the full page text and store the embedding in the pgvector `pages` column
 
-    - Save keyword-occurences-page_id triplets to database
+  - For each text chunk (or the whole page, depending on memory usage in testing) using [Apache Lucene]("https://lucene.apache.org/core/"):
+    - Tokenize the page text.
+    - Lowercase terms.
+    - Remove punctuation and stop words.
+    - Apply stemming or lemmatization.
+    - Index the resulting terms with Lucene.
+
+  - Save keyword-occurences-page_id triplets to database
--- services/indexer/src/main/java/com/nicholasgarcia/ohana/indexer/App.java
+++ services/indexer/src/main/java/com/nicholasgarcia/ohana/indexer/App.java
@@ -2,7 +2,11 @@ package com.nicholasgarcia.ohana.indexer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.time.Duration;
 
 public class App {
 
@@ -15,17 +19,72 @@ public class App {
         final String DB_NAME = System.getenv("DB_NAME");
         final String JDBC_URL = "jdbc:postgresql://" + DB_HOST + ":" + DB_CONTAINER_PORT + "/" + DB_NAME;
 
-        Connection connection;
+        Connection connection = null;
         try {
             System.out.println("Connecting to PostgreSQL...");
             connection = DriverManager.getConnection(JDBC_URL, DB_USERNAME, DB_PASSWORD);
             System.out.println("Connected to PostgresSQL successfully.");
 
-            connection.close();
+            while (true) {
+                int PAGE_ID = -1, SITE_ID = -1;
+                String PAGE_URL = "", RESPONSE_BODY = "";
+                PreparedStatement stmt = connection.prepareStatement(
+                        "SELECT indexer_queue.page_id, indexer_queue.site_id, pages.link, pages.response_body FROM indexer_queue LEFT JOIN pages ON pages.id = indexer_queue.page_id ORDER BY pages.id ASC LIMIT 1;"
+                );
+                ResultSet result = stmt.executeQuery();
+
+                boolean isIndexerQueueEmpty = !(result.next());
+                if (isIndexerQueueEmpty) {
+                    Duration.ofSeconds(1).wait();
+                    // TODO: then get the page that has been indexed the longest time ago.
+
+                    stmt.close();
+                    result.close();
+                    continue;
+                }
+
+                PAGE_ID = result.getInt("page_id");
+                SITE_ID = result.getInt("site_id");
+                PAGE_URL = result.getString("link");
+                RESPONSE_BODY = result.getString("response_body");
+
+                boolean isResultInvalid = PAGE_ID == -1 || SITE_ID == -1 || PAGE_URL.equals("");
+                if (isResultInvalid) {
+                    throw new Exception("invalid row values returned from database query");
+                }
+
+                System.out.println(RESPONSE_BODY);
+
+                // TODO: Extract text chunks from the page
+                // TODO: Compile the chunks into a single full text source too
+                // TODO: Run [sentence-transformers/all-MiniLM-L6-v2]("https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2") in ONNX runtime
+                stmt.close();
+                result.close();
+                break;
+            }
+
         } catch (SQLException e) {
-            System.out.println("connect to postgresql failed: " + e.getMessage());
+            System.out.println("database/SQL error: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(e.getErrorCode());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
             e.printStackTrace();
             System.exit(1);
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException exc) {
+                System.out.println("database/SQL error: " + exc.getMessage());
+                exc.printStackTrace();
+                System.exit(exc.getErrorCode());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+                System.exit(1);
+            }
         }
     }
 }
