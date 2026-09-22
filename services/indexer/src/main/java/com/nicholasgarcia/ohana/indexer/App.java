9fb6b79791ef71de272b5cec9370c1d9f90d7671
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-09-22 01:13:14 -0600

    build: initialize Maven project

---
+++ services/indexer/src/main/java/com/nicholasgarcia/ohana/indexer/App.java
@@ -0,0 +1,10 @@
+package com.nicholasgarcia.ohana.indexer;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
